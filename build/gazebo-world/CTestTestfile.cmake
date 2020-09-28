404d7f093961d558a247958f675ac63d4323205c
SALVO-ROS <borgesisalvo@gmail.com>
2020-09-28 19:10:52 +0200

    gazebo first commit

---
+++ build/gazebo-world/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/salvo/simulation_ws/src/gazebo-world
+# Build directory: /home/salvo/simulation_ws/build/gazebo-world
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ build/robot-wheels/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/salvo/simulation_ws/src/robot-wheels
+# Build directory: /home/salvo/simulation_ws/build/robot-wheels
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
